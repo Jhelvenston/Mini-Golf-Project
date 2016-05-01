d4abccb98b0ca7e8a789e7b7d569181117e7ebd0
Thomas Schlicher <'tschlicher@yahoo.com'>
2016-05-01 13:47:10 -0500

    Setting up doc comments before designing my method (enterScore).

--- Round/src/round/Round.java
+++ Round/src/round/Round.java
@@ -21,8 +21,14 @@ public class Round
        return 0;
    }
    
-   public void enterScore()
+   public void enterScore(int strokes)
    {
+       //This method will take in a score for the next hole.
+       //So what ever score a player has at then end of the whole will be input here.
+       //No holes can be skipped and they must all be played in order.
+       
+       //For each player, each time the ball is hit without making it in:
+       //1 point will be added until the ball has landed in the hole.
        
    }
    
