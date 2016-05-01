049ed481edb6a42450f7d56c7a5dbae837137d89
Thomas Schlicher <'tschlicher@yahoo.com'>
2016-05-01 08:58:35 -0500

    Just set up skeleton for our JUnit test.

---
+++ Round/src/round/Round.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package round;
+
+/**
+ *
+ * @author thomas
+ */
+public class Round 
+{
+   public int currentScore()
+   {
+       return 0;
+   }
+   
+   public int numberOfHolesPlayed()
+   {
+       return 0;
+   }
+   
+   public void enterScore()
+   {
+       
+   }
+   
+   public int[] scoreDetail()
+   {
+       int[] madeUp = {};
+      return madeUp;
+   }
+}
