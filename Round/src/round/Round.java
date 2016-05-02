ee5a9b00e39f5e356c387295ba43da364f6447b8
Thomas Schlicher <'tschlicher@yahoo.com'>
2016-05-02 09:29:23 -0500

    Creating detailed comments while adding fields.

--- Round/src/round/Round.java
+++ Round/src/round/Round.java
@@ -13,12 +13,30 @@ public class Round
 {
    public int currentScore()
    {
-       //cuurentScore method will calculate the players total score so far
+       //cuurentScore method will calculate the players score so far
        //(above or below par).
        
        //above par - anything above the maximum number of attempts on a hole
        
        //below par - anything below the minimum number of attempts on a hole
+       
+       //to get users current score:
+       //we need to count up the number of pars for holes played: i.e. hole one
+       // is a par 4, hole two is a par 3 so total is 7 pars played so far....
+       
+       //holesPlayed - used to get the number of holes played
+       int holesPlayed;
+       
+       //this will be used to help calculate the users current score.
+       int parTotal;
+       
+       //this will be used to add up all strokes a customer has done up to a hole
+       //at this point if the customer has lets say 6 strokes on the first two
+       //holes and the total combined par is 7 for the first two holes, then the user 
+       //is -1 under par.
+       int attemptsPerHole;
+       
+       
        
        return 0;
    }
