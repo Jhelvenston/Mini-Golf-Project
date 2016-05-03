e24b96abafddb3f9c2212be276dadc7360c743a4
dMatheny2016 <djcy2012@gmail.com>
2016-05-03 19:24:33 -0400

    Setting up skeletons

---
+++ MiniGolf/src/minigolf/Round.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minigolf;
+
+/**
+ *
+ * @author Dan
+ */
+public class Round {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    //This will keep track of the scores
+    
+    public int currentScore(){
+        int score=0;
+        
+        return score;
+    }
+}
