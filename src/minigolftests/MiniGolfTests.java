74f85a3864d724dc958ae19d6853569a76a796cc
James <James@Sphinx>
2016-05-04 16:21:10 -0400

    James Helvenston - Added stub methods and tests

---
+++ src/minigolftests/MiniGolfTests.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minigolftests;
+
+/**
+ *
+ * @author James
+ */
+public class MiniGolfTests {
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
+
+class Round {
+    
+    public int currentScore(){
+        return 0;
+    }
+    
+    public int holesPlayed(){
+        return 0;
+    }
+    
+    public void enterScore( int strokes ){
+        
+    }
+    
+    public int[] scoreDetail(){
+        int i[] = {0};
+        return i;
+    }
+}
